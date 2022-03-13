fb4fe065b13ab4ff4143f51158d110fc37046427
arega24 <99811425+arega24@users.noreply.github.com>
2022-03-13 23:45:45 +0000

    Update initial-setup-script.sh

--- initial-setup-script.sh
+++ initial-setup-script.sh
@@ -12,7 +12,8 @@ apt upgrade -y
 
 # Add base packages
 apt install unzip tar rar picom dunst bspwm polybar rofi kitty sxhkd git piper lxappearance gnome-screenshot feh sxiv nemo -y
-apt install papirus-icon-theme -y
+apt install papirus-icon-theme numix-icon-theme paper-icon-theme deepin-icon-theme sugar-icon-theme -y
+apt install xcursor-themes breeze-cursor-theme -y
 apt install fonts-font-awesome -y
 apt install lightdm slick-greeter lightdm-settings xorg -y
 apt install snapd vim -y
