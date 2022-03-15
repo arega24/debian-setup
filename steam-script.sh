b9d4adbef581621ed17ea7a8c093def55505d8bb
arega24 <99811425+arega24@users.noreply.github.com>
2022-03-15 21:56:18 +0000

    Update steam-script.sh

--- steam-script.sh
+++ steam-script.sh
@@ -8,3 +8,4 @@ dpkg --add-architecture i386
 apt update
 apt install steam
 apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
+apt install libnvidia-glcore:i386 libnvidia-eglcore:i386 libglx-nvidia0:i386 libgl1-nvidia-glvnd-glx:i386
