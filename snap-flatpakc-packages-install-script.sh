4eb1b872c4993716d2bc5a894ab49aac30a56289
arega24 <99811425+arega24@users.noreply.github.com>
2022-03-16 19:06:03 +0000

    Update snap-flatpakc-packages-install-script.sh

--- snap-flatpakc-packages-install-script.sh
+++ snap-flatpakc-packages-install-script.sh
@@ -2,10 +2,10 @@
 
 snap install core
 snap install brave tradingview foobar2000
+snap install code --classic
 
 flatpak install flathub org.standardnotes.standardnotes
 flatpak install flathub com.todoist.Todoist
-flatpak install flathub com.visualstudio.code
 flatpak install flathub org.blender.Blender
 flatpak install flathub com.obsproject.Studio
 flatpak install flathub com.spotify.Client
@@ -13,7 +13,6 @@ flatpak install flathub com.discordapp.Discord
 flatpak install flathub org.kde.krita
 flatpak install flathub io.github.Qalculate
 flatpak install flathub org.videolan.VLC
-flatpak install flathub com.usebottles.bottles
 
 apt install lutris
 
