7cd3da5d29b5c06634cb119229b0d6fe07149bb9
arega24 <99811425+arega24@users.noreply.github.com>
2022-04-11 21:30:56 +0100

    Update initial-setup-script.sh

--- initial-setup-script.sh
+++ initial-setup-script.sh
@@ -8,7 +8,7 @@ apt upgrade -y
 # Add base packages
 apt install unzip tar rar picom dunst libnotify-bin bspwm polybar rofi kitty sxhkd git piper lxappearance gnome-screenshot feh sxiv nemo redshift -y
 apt install papirus-icon-theme numix-icon-theme paper-icon-theme deepin-icon-theme sugar-icon-theme adwaita-icon-theme -y
-apt install fonts-font-awesome -y
+apt install fonts-font-awesome fonts-roboto -y
 apt install lightdm slick-greeter lightdm-settings xorg -y
 apt install snapd vim -y
 apt install pulseaudio alsa-utils pavucontrol lollypop playerctl -y
@@ -28,3 +28,4 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 # lightdm
 systemctl enable lightdm
+
